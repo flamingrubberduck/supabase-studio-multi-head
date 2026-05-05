32aaa3e1c0a017a201496f1841f4e4793f90361b
sonvh1 <sonvh1@openmeets.co>
2026-05-05 11:04:38 +0700

    update team and billing

--- apps/studio/pages/api/platform/organizations/[slug]/members/invitations.ts
+++ apps/studio/pages/api/platform/organizations/[slug]/members/invitations.ts
@@ -5,7 +5,7 @@ import apiWrapper from '@/lib/api/apiWrapper'
 export default (req: NextApiRequest, res: NextApiResponse) => apiWrapper(req, res, handler)
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') return res.status(200).json([])
+  if (req.method === 'GET') return res.status(200).json({ invitations: [] })
   res.setHeader('Allow', ['GET'])
   res.status(405).json({ data: null, error: { message: `Method ${req.method} Not Allowed` } })
 }
--- apps/studio/pages/api/platform/organizations/[slug]/roles.ts
+++ apps/studio/pages/api/platform/organizations/[slug]/roles.ts
@@ -5,7 +5,7 @@ import apiWrapper from '@/lib/api/apiWrapper'
 export default (req: NextApiRequest, res: NextApiResponse) => apiWrapper(req, res, handler)
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') return res.status(200).json({ roles: [] })
+  if (req.method === 'GET') return res.status(200).json({ org_scoped_roles: [], project_scoped_roles: [] })
   res.setHeader('Allow', ['GET'])
   res.status(405).json({ data: null, error: { message: `Method ${req.method} Not Allowed` } })
 }
